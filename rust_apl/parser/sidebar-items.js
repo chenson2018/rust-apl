b8b354eac6f80492d4ac4c504f194c76bdf903c0
chenson2018 <chenson2018@users.noreply.github.com>
2022-06-24 01:55:28 +0000

    deploy: 46ccc8aca27ec444aad11e10551f3e61a19eb269

--- rust_apl/parser/sidebar-items.js
+++ rust_apl/parser/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Parser","A struct representing a parser. This operates much like rust_apl::scanner::Scanner, but instead of transforming raw characters into tokens, this takes those tokens and builds a tree representing an expression."]]});
+initSidebarItems({"static":[["DYADIC_ONLY",""]],"struct":[["Parser","A struct representing a parser. This operates much like rust_apl::scanner::Scanner, but instead of transforming raw characters into tokens, this takes those tokens and builds a tree representing an expression."]]});
